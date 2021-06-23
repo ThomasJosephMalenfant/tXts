6bd437bc0b9e73996c4a0aa5534f84719df55d7b
Thomas Malenfant <thomas.joseph.malenfant@gmail.com>
2021-06-23 11:13:04 -0400

    Fix print_r --> print()

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<title></title>
 </head>
 <body>
-	<p><a href="<?php print_r( URL_PREFIX . '/heures/index.php' ); ?>">Heures</a></p>
-	<p><a href="<?php print_r( URL_PREFIX . '/parole/index.php' ); ?>">Parole</a></p>
+	<p><a href="<?php print( URL_PREFIX . '/heures/index.php' ); ?>">Heures</a></p>
+	<p><a href="<?php print( URL_PREFIX . '/parole/index.php' ); ?>">Parole</a></p>
 </body>
 </html>
