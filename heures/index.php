d9d21103bdb8f9f6023f9b34332960b8f88aa7dd
Thomas Malenfant <thomas.joseph.malenfant@gmail.com>
2021-06-23 11:02:08 -0400

    Concat URL_PREFIX

--- heures/index.php
+++ heures/index.php
@@ -356,8 +356,7 @@ if ( $semaine_nb = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT) ) {
         </head>
         <body>
             <h2>
-                <?php print_r( URL_PREFIX ) ; ?>
-                <a href="<?php print_r( URL_PREFIX + '/heures/index.php?page=1' ); ?>" > Cette semaine </a><br>
+                <a href="<?php print_r( URL_PREFIX . '/heures/index.php?page=1' ); ?>" > Cette semaine </a><br>
                 <a href="/heures/index.php?page=2"> Semaine prochaine </a><br>
                 <a href="/heures/index.php?page=3"> Dans 2 semaines </a><br>
                 <a href="/heures/index.php?page=4"> Dans 3 semaines </a><br>
