f0d037f0fd6ba1448126529bd9cb39bb56bf84b2
Thomas Malenfant <thomas.joseph.malenfant@gmail.com>
2021-07-08 11:02:23 -0400

    Externaliser styles.css sur parole.php

--- parole/index.php
+++ parole/index.php
@@ -12,33 +12,7 @@ if ( $references = filter_input(INPUT_POST, 'reference', FILTER_SANITIZE_STRING)
         <meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-		<style>
-			p {
-				font-size:20px;
-			}
-			.titre1 {
-				font-weight: 900;
-				font-size: 48px;
-				margin: 20px 0 5px;
-			}.titre2 {
-				font-weight: 700;
-				font-size: 38px;
-				margin: 20px 0 5px;
-			}
-			.titre3 {
-				font-weight: 500;
-				font-size: 22px;
-				margin: 20px 0 5px;
-			}
-			.verse_number {
-				vertical-align: super;
-				font-size: 8px;
-			}
-			.fin {
-				width:100% ;
-				page-break-after:always;
-			}
-		</style>
+		<link rel="stylesheet" href="./styles.css">
  		<title><?php print $reference ; ?></title>
 	</head>
 	<body>
@@ -73,14 +47,19 @@ if ( $references = filter_input(INPUT_POST, 'reference', FILTER_SANITIZE_STRING)
 	<html>
 	<head>
         <meta charset="UTF-8" />
+		<link rel="stylesheet" href="./styles.css">
  		<title>Choisir le texte</title>
 	</head>
 	<body>
-		<form method="post" autocomplete="off">
-			<label for="reference">Référence finale :</label><br>
-			<input type="text" id="reference" name="reference"><br>
-			<input type="submit" value="Envoyer">
-		</form>
+		<div class="centrement">
+			<div>
+				<form method="post" autocomplete="off">
+					<label for="reference">Référence :</label><br>
+					<input type="text" id="reference" name="reference"><br>
+					<input type="submit" value="Envoyer">
+				</form>
+			</div>
+		</div>
 	</body>
 	</html>
 	<?php
