494d8edd7d0eb5c9828a63d1c10b197ffae02ba3
Thomas Malenfant <thomas.joseph.malenfant@gmail.com>
2021-07-09 13:22:21 -0400

    Début structuration /rituel/index.php

--- rites/index.php
+++ rites/index.php
@@ -3,66 +3,17 @@ require '../env.php';
 require "../parole/BibleClass.php" ;
 
 ob_start();
+// NEXT : Structure triple :
+//			if (isset(schema)) { 
+//			} elseif (isset(rituel)) {
+//			} else { choix de rituel }
 
-if ( $references = filter_input(INPUT_POST, 'reference', FILTER_SANITIZE_STRING) ) { 
-	$affichage_versets = isset($_POST['affichage_versets']) ? TRUE : FALSE ;
-
-	?>
-	<!DOCTYPE html>
-	<html>
-	<head>
-        <meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-		<link rel="apple-touch-icon" sizes="180x180" href="../icones/apple-touch-icon.png">
-		<link rel="icon" type="image/png" sizes="32x32" href="../icones/favicon-32x32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="../icones/favicon-16x16.png">
-		<link rel="stylesheet" href="./rites.css">
-		<script src="./rites.js" defer></script>  
- 		<title><?php print $reference ; ?></title>
-	</head>
-	<body>
-	<?php
-	print '<div id="pericopes">' ;
-	foreach (explode("|",$references) as $reference) {
-		$pericope = new Pericope(trim($reference)) ;
-		if (! $pericope->erreurs ) {
-			?>
-				<h1 class="titre1"><?php print $pericope->titre ; ?></h1>
-				<h2 class="titre3"><?php print $reference ; ?></h1>
-				<?php
-					$no_chapitre = 0 ;
-					$no_verset = 0 ;
-					print '</br><div class="editeur" contentEditable="true"><pre>' ;
-					foreach ($pericope->versets as $verset) {
-						$old_chap = $no_chapitre ;
-						$no_chapitre = $verset["chapitre"] ;
-						$no_verset =  $verset["verset"] ;
-						if ($no_chapitre > $old_chap) {
-							$verse_number = $no_chapitre . ", " . $no_verset ;
-						} else { $verse_number = $no_verset ; }
-						if ($affichage_versets){
-							print '<p class="verset_pericope" ><span class = "verse_number" >' . $verse_number . " </span>" . $verset["texte"] . "</p>";
-						} else {
-							print '<p class="verset_pericope" >' . $verset["texte"] . "</p>";
-						}
-					}
-					print '</pre></div>' ;
-		} else {
-			?>
-			<h1 class="titre1"> Erreurs </h1>
-			<?php
-			foreach ($pericope->erreurs as $erreur) {
-				print '<p>' . $erreur . '</p>' ;
-			}
-		}
-	}
-	print '</div>';
-	?>
-	</body>
-	</html>
-	<?php
+if ( filter_input(INPUT_POST, 'schema', FILTER_SANITIZE_STRING) ) { 
+	// Output du rituel
+} elseif (filter_input(INPUT_POST, 'rituel', FILTER_SANITIZE_STRING)){
+	// Output questionnaire schema
 } else {
+	// Output choix du rituel
 	?>
 	<!DOCTYPE html>
 	<html>
