e61cdcdec8a54a9897f6ea5ba0b8a42ee362e4bb
Thomas Malenfant <thomas.joseph.malenfant@gmail.com>
2021-07-27 10:31:40 -0400

    Début test horaires

---
+++ horaires/horaires.js
@@ -0,0 +1,10 @@
+var calendar = new Calendar('#calendar', {
+    defaultView: 'month',
+    taskView: true,
+    template: {
+      monthDayname: function(dayname) {
+        return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
+      }
+    }
+  });
+  
